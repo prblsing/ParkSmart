379b72199620689b2d1e0ae0e2353db63be9c587
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-29 22:36:17 +0530

    Update model_config.py

--- park_smart/config/model_config.py
+++ park_smart/config/model_config.py
@@ -1,47 +1,36 @@
 # model_config.py
-import os
 import torch
+import os
 import gdown
-import streamlit as st
 
-# Define the path for the YOLOv5 model
-MODEL_DIR = 'models'
-MODEL_PATH = os.path.join(MODEL_DIR, 'pretrained_model_state_dict.pt')
+# Path to the YOLOv5 model file
+MODEL_PATH = 'models/pretrained_model_state_dict.pt'
+DRIVE_URL = 'https://drive.google.com/uc?id=1zWhDEF8WkDDRrrGeu23ySXWAYi1QKWfg'
+
 
 def download_model():
     """
-    Downloads the YOLO model from a Google Drive URL if not already present.
+    Downloads the YOLOv5 model from Google Drive if it does not exist.
     """
-    drive_url = st.secrets["DRIVE_URL"]  # Correct usage for Streamlit secrets
-    model_path = os.path.join('models', 'pretrained_model_state_dict.pt')
-    
-    if not drive_url:
-        logging.error("DRIVE_URL is not set. Please set it in Streamlit secrets or environment variables.")
-        raise ValueError("DRIVE_URL is not set.")
-    
-    if not os.path.exists('models'):
-        os.makedirs('models')
+    if not os.path.exists(MODEL_PATH):
+        # Ensure the models directory exists
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
 
-    if not os.path.exists(model_path):
-        try:
-            logging.info(f"Attempting to download model from {drive_url}")
-            gdown.download(drive_url, model_path, quiet=False)
-        except Exception as e:
-            logging.error(f"Failed to download the model: {e}")
-            raise
+        # Download model from Google Drive
+        print(f"Downloading YOLOv5 model to {MODEL_PATH}...")
+        gdown.download(DRIVE_URL, MODEL_PATH, quiet=False)
+    else:
+        print(f"Model already exists at {MODEL_PATH}.")
+
 
 def load_yolo_model():
     """
-    Loads the YOLOv5 model after downloading it if necessary.
+    Loads the YOLOv5 model after downloading if necessary.
     """
     download_model()
 
-    if os.path.exists(MODEL_PATH):
-        # Load the model's state dict
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-        model.load_state_dict(torch.load(MODEL_PATH))
-        model.eval()  # Set the model to evaluation mode
-        return model
-    else:
-        st.error("Model file does not exist. Please check the model download process.")
-        return None
+    # Load the model's state dict
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.eval()  # Set the model to evaluation mode
+    return model
