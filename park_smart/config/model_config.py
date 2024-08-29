e8c09dcf32cf38a3aaeeaf48ae0f6af19dfa93aa
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-29 22:15:36 +0530

    Update model_config.py

--- park_smart/config/model_config.py
+++ park_smart/config/model_config.py
@@ -8,17 +8,6 @@ import streamlit as st
 MODEL_DIR = 'models'
 MODEL_PATH = os.path.join(MODEL_DIR, 'pretrained_model_state_dict.pt')
 
-def get_drive_url():
-    """
-    Fetch the Google Drive URL from Streamlit secrets.
-    """
-    try:
-        drive_url = st.secrets["DRIVE_URL"]
-        return drive_url
-    except KeyError:
-        st.error("Google Drive URL not found in Streamlit secrets. Please set the 'DRIVE_URL' in your Streamlit secrets.")
-        return None
-
 def download_model():
     """
     Downloads the YOLOv5 model from Google Drive if it does not exist.
@@ -27,13 +16,13 @@ def download_model():
         # Ensure the models directory exists
         os.makedirs(MODEL_DIR, exist_ok=True)
 
-        # Download the model from Google Drive
-        drive_url = get_drive_url()
+        # Get the Google Drive URL from Streamlit secrets
+        drive_url = st.secrets.get("DRIVE_URL", None)
         if drive_url:
             print(f"Downloading YOLOv5 model from {drive_url} to {MODEL_PATH}...")
             gdown.download(drive_url, MODEL_PATH, quiet=False)
         else:
-            print("Model download URL not provided.")
+            st.error("Drive URL is not set in Streamlit secrets.")
     else:
         print(f"Model already exists at {MODEL_PATH}.")
 
@@ -43,8 +32,12 @@ def load_yolo_model():
     """
     download_model()
 
-    # Load the model's state dict
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-    model.load_state_dict(torch.load(MODEL_PATH))
-    model.eval()  # Set the model to evaluation mode
-    return model
+    if os.path.exists(MODEL_PATH):
+        # Load the model's state dict
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        model.load_state_dict(torch.load(MODEL_PATH))
+        model.eval()  # Set the model to evaluation mode
+        return model
+    else:
+        st.error("Model file does not exist. Please check the model download process.")
+        return None
